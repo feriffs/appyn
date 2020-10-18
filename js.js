ba8977f4b862190e5454cf1c3b1fe775db4decb4
feriffs <68251503+feriffs@users.noreply.github.com>
2020-10-19 00:31:46 +0700

    Update js.js

--- js.js
+++ js.js
@@ -46,7 +46,7 @@ if(text_ === true){
 			  $('body').toggleClass('toggle-nav');
 			  if( $(this).find('i').hasClass('fa-bars') ) {
 				  $(this).find('i').attr('class', 'fa fa-close');
-				  $('#menu-mobile').animate({right: '20%'}, "slow");
+				  $('#menu-mobile').removeAttr('style');
 			  } else {
 				  $(this).find('i').attr('class', 'fa fa-bars');
 				  $('#menu-mobile').hide();
